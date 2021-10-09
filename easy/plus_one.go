1e4e04184a92bb5f2d13950ca34281e712adc1b9
kiriljiang <kiriljiang@tencent.com>
2021-10-09 13:22:00 +0800

    easy, array: 数组加一

---
+++ easy/plus_one.go
@@ -0,0 +1,63 @@
+package easy
+
+//给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+//
+// 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+//
+// 你可以假设除了整数 0 之外，这个整数不会以零开头。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：digits = [1,2,3]
+//输出：[1,2,4]
+//解释：输入数组表示数字 123。
+//
+//
+// 示例 2：
+//
+//
+//输入：digits = [4,3,2,1]
+//输出：[4,3,2,2]
+//解释：输入数组表示数字 4321。
+//
+//
+// 示例 3：
+//
+//
+//输入：digits = [0]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= digits.length <= 100
+// 0 <= digits[i] <= 9
+//
+// Related Topics 数组 数学 👍 765 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func plusOne(digits []int) []int {
+	// deepcopy
+	//length := len(digits)
+	//ans := make([]int, length)
+	//copy(ans, digits)
+	digits[len(digits)-1] += 1
+	carryBit := 0
+	for length := len(digits) - 1; length >= 0; length-- {
+		temp := digits[length] + carryBit
+		digits[length] = temp % 10
+		carryBit = temp / 10
+	}
+	if carryBit == 1 {
+		digits = append([]int{1}, digits...)
+	}
+	return digits
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
